814e5ab3d14da5a355f0b05c1465367bbe21e439
ishini22 <107682188+ishini22@users.noreply.github.com>
2023-10-16 19:40:43 +0530

    Add files via upload

---
+++ storage/framework/views/5cf8068f4731b1b75f074f001f41e410.php
@@ -0,0 +1,13 @@
+
+
+<?php $__env->startSection('content'); ?>
+<div class="container mt-5">
+    <h1>Wishlist Page</h1>
+    <p>This is your wishlist content. You can customize it as needed.</p>
+</div>
+
+<script>
+    // Add Alpine.js behavior for this page here
+</script>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\User\Downloads\project2\resources\views/wishlist.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/74dedef14c8e58f92db2a7a47f1cbde2.php
@@ -0,0 +1,42 @@
+<?php if (isset($component)) { $__componentOriginal9ac128a9029c0e4701924bd2d73d7f54 = $component; } ?>
+<?php $component = App\View\Components\AppLayout::resolve([] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? (array) $attributes->getIterator() : [])); ?>
+<?php $component->withName('app-layout'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag && $constructor = (new ReflectionClass(App\View\Components\AppLayout::class))->getConstructor()): ?>
+<?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+
+<section class="max-w-4xl p-6 mx-auto bg-grey-300 rounded-md shadow-md dark:bg-gray-800 mt-20">
+    <h1 class="text-xl font-bold text-black capitalize dark:text-white ">Edit Users</h1><br>
+<form method='post' action="<?php echo e(url('updateuser')); ?>">
+<?php echo csrf_field(); ?>
+<input type="hidden" name="id" value="<?php echo e($user->id); ?>"> 
+<div class="mb-6">
+<label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
+<input type="text" name="name" value="<?php echo e($user->name); ?>" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name" required>
+</div>
+
+<div class="mb-6">
+<label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+<input type="email" name="email" value="<?php echo e($user->email); ?>" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required>
+</div>
+
+<div class="mb-6">
+<label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+<input type="password" name="password" value="<?php echo e($user->email); ?>"  id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+</div>
+
+
+<button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+<a href="<?php echo e(url('/redirect')); ?>"><button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Back</button></a>
+</form>
+</section>
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal9ac128a9029c0e4701924bd2d73d7f54)): ?>
+<?php $component = $__componentOriginal9ac128a9029c0e4701924bd2d73d7f54; ?>
+<?php unset($__componentOriginal9ac128a9029c0e4701924bd2d73d7f54); ?>
+<?php endif; ?>
+<?php /**PATH C:\xampp\htdocs\project2\resources\views/editusers.blade.php ENDPATH**/ ?>
